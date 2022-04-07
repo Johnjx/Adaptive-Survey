cc629455ad0f27576c61002083e4b8a3dd6f180e
John Ruddock <john.jxr@pm.me>
2022-04-07 17:05:03 -0400

    added hide class to survey button, added classes to response buttons, modified JS to have one function that running the survey program instead of separate functions for each response. Still need to modify survey function to pull all possible questions from survey bank. Also added comments throughout code.

--- index.js
+++ index.js
@@ -1,50 +1,13 @@
-// const questions = {
-//     question1:"This is question 1?",
-//     question2:"This is question 2?",
-//     question3:"This is question 3?",
-//     question4:"This is question 4?",
-//     question5:"This is question 5?",
-//     question6:"This is question 6?",
-//     question7:"This is question 7?",
-//     question8:"This is question 8?"
-// }
-
-const questions = [
-    "This is question 1?",
-    "This is question 2?",
-    "This is question 3?",
-    "This is question 4?",
-    "This is question 5?",
-    "This is question 6?",
-    "This is question 7?",
-    "This is question 8?"
-]
-
-// testing
-// const h1 = document.querySelector("h1");
-// const h1class = "h1class";
-
-const p = document.querySelector("p");
-const input = document.querySelector("input");
-const start = document.getElementById("startSurvey");
-const cough = "cough";
-
-const yesButton = document.getElementById("yesButton");
-const noButton = document.getElementById("noButton");
-
-const newArr = [];
-
-start.addEventListener('click', () => {
-    if (input.value === cough) {
-        newArr.push(questions[0]);
-        p.innerHTML = questions[0];
-    }
-
-});
-
-
-
-const answer = Math.floor(Math.random() * 8 + 1);
+const questions = {
+    question1:"This is question 1?",
+    question2:"This is question 2?",
+    question3:"This is question 3?",
+    question4:"This is question 4?",
+    question5:"This is question 5?",
+    question6:"This is question 6?",
+    question7:"This is question 7?",
+    question8:"This is question 8?"
+}
 
 // yesButton.addEventListener('click', () => {
 //     if (p.innerHTML === questions.question1) {
@@ -57,18 +20,111 @@ const answer = Math.floor(Math.random() * 8 + 1);
     
 // })
 
-yesButton.addEventListener('click', () => {
-    for (let i = 0; i < newArr.length; i++)
-    if (p.innerHTML.includes(newArr[i])) {
-        p.innerHTML = questions[answer];
+// Changed to Array for testing
+// const questions = [
+//     "This is question 1?",
+//     "This is question 2?",
+//     "This is question 3?",
+//     "This is question 4?",
+//     "This is question 5?",
+//     "This is question 6?",
+//     "This is question 7?",
+//     "This is question 8?"
+// ]
+
+// const answer = Math.floor(Math.random() * 8 + 1);
+
+
+// yesButton.addEventListener('click', () => {
+//     for (let i = 0; i < newArr.length; i++)
+//     if (p.innerHTML.includes(newArr[i])) {
+//         p.innerHTML = questions[answer];
         
-    } 
+//     } 
     
-})
+// })
 
-noButton.addEventListener('click', () => {
+// noButton.addEventListener('click', () => {
 
-})
+// })
 
-console.log(newArr)
+
+// selecting my elements
+// p will be changed to different questions
+const p = document.querySelector("p");
+//save the word cough and input element for if statment
+const cough = "cough";
+const input = document.querySelector("input");
+// button that triggers first question load in
+const start = document.getElementById("startSurvey");
+// response buttons to trigger new question load in
+const yesButton = document.getElementById("yesButton");
+const noButton = document.getElementById("noButton");
+const options = document.querySelectorAll(".options");
+const option1 = 'option1';
+const option2 = 'option2';
+
+// flow
+// already mock set up - user input of 'cough' into the symptom field and they click 'start survey'
+// first question loads in to 'p' element space
+// if user responds by clicking yes button then a question loads into 'p'
+// or if user responds by clicking no button then a different question loads into 'p'
+// this happens until there are no new questions able to be loaded into 'p'
+
+// solution
+
+const newArr = [];
+
+// first load in
+
+start.addEventListener('click', () => {
+    if (input.value === cough) {
+        start.classList.add('hideSurvey');
+        newArr.push(questions.question1);
+        p.innerHTML = questions.question1;  
+    }
+});
+
+// rest of the load ins based off yes or no response
+// loop through newArr
+// 
+// check p.inner against newArr[i]
+// if ===, for (let x in questions) { if (questions[x] !== p.inner) set p and push to newAarr}
+// 
+// 
+
+const surveyGo = () => {
+    options.forEach(response =>{
+        response.addEventListener('click', (evt) => {
+            for (let i = 0; i < newArr.length; i++) {
+                if (evt.target.className.includes(option1) && p.innerHTML === newArr[i]) {
+                    p.innerHTML = questions.question2;
+                    
+                } else if (evt.target.className.includes(option2) && p.innerHTML === newArr[i]) {
+                    p.innerHTML = questions.question6;
+                }
+            } 
+           newArr.push(p.innerHTML);
+        });
+    });
+}
+
+surveyGo();
+
+// const surveyGo = () => {
+//     options.forEach(response =>{
+//         response.addEventListener('click', (evt) => {
+
+//             if (p.innerHTML === questions.question1 && evt.target.className.includes(option1)) {
+//                 p.innerHTML = questions.question2;
+//                 newArr.push(questions.question2);
+//             } else if (p.innerHTML === questions.question1 && evt.target.className.includes(option2)) {
+//                 p.innerHTML = questions.question5;
+//                 newArr.push(questions.question5);
+//             }
+//         });
+//     });
+// }
+
+// surveyGo();
 
